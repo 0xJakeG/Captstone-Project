f316298061e55338b3362a0030ec34c6bb7322d9
0xJakeG <jakemray2020@gmail.com>
2023-02-13 15:32:38 -0500

    Changed file format added src folder with js files

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <ul class="flex-row">
+            <li><a class="active" href="#recipes">My Recipes</a></li>
+            <li><a href="#addrecipe">Add Recipe</a></li>
+            <li><a href="#signupin">Sign In</a></li>
+        </ul>
+    </nav>
+
+}
+
