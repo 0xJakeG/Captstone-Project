98914686b072f0994789927e26c4b6d43d83b52b
0xJakeG <jakemray2020@gmail.com>
2023-02-21 21:59:54 -0500

    Revert "sql"
    This reverts commit 43eccaa0faafd79661be2ea349d4121660966ea4.

--- app.js
+++ app.js
@@ -3,26 +3,6 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const Route = require('./routes/mainRoute');
 
-const mysql = require("mysql");
-
-const db = mysql.createConnection({
-    host: "awseb-e-epz4ed3tmg-stack-awsebrdsdatabase-uz3xxyihfosx.cs6g7v4x3uz2.us-east-1.rds.amazonaws.com",
-    port: "3306",
-    user: "JakeAdmin",
-    password: "69LgU84Bta8RZJr",
-    database: "ebdb",
-
-})
-
-db.connect((err) => {
-    if (err) {
-        console.log(err.message);
-        return;
-    }
-    console.log("Database connected");
-
-});
-
 app = express();
 
 app.set('view engine', 'ejs');
