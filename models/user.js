7bf069b79b172bf803fe5b03c21cf181286094b3
0xJakeG <jakemray2020@gmail.com>
2023-03-16 19:25:13 -0400

    bolier plate user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 const { sequelize } = require(".");
 
 module.exports = (sequelize,DataTypes) => {
-    const user = sequelize.define("user", )
-}
+    const user = sequelize.define("user", {
+    
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            valiidate: {
+                notEmpty: true
+            },
+        },
+    
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            valiidate: {
+                notEmpty: true
+            }
+        },
+
+        test: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            valiidate: {
+                notEmpty: true
+            }
+        }
+});
+
+    return user;
+};
