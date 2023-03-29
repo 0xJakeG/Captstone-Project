c5eed9e657f93178206e22baf40d5c63837d9e7e
0xJakeG <jakemray2020@gmail.com>
2023-03-29 12:55:32 -0400

    update config.js to use lambda

--- config/config.js
+++ config/config.js
@@ -1,24 +1,45 @@
 require('dotenv').config();
-module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({region: 'us-east-1'});
+
+async function getEnvVars() {
+  const params = {
+    FunctionName: 'env',
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({}),
+  };
+
+  const response = await lambda.invoke(params).promise();
+  const envVars = JSON.parse(response.Payload);
+
+  return envVars;
 }
+
+module.exports = async () => {
+  const envVars = await getEnvVars();
+
+  return {
+    "development": {
+      "username": envVars.DB_USERNAME,
+      "password": envVars.DB_PASSWORD,
+      "database": envVars.DB_DATABASE,
+      "host": envVars.DB_HOST,
+      "dialect": envVars.DB_DIALECT
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  };
+};
