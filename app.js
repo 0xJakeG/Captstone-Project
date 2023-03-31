155367d83d4b2081d3212fa507b35a79d88a9e91
0xJakeG <jakemray2020@gmail.com>
2023-03-30 21:23:32 -0400

    add const from seq_v2 app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const Route = require('./routes/mainRoute');
+const db = require('./sequelize/models');
+const { request } = require('express');
+const bodyParser = require('body-parser');
+const userModel = require('./sequelize/models/user');
 
+const {user} = require ("./sequelize/models");
+require('dotenv').config();
 app = express();
 
 app.set('view engine', 'ejs');
