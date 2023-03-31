e53747b26225eea5da9ce36aa5cf617764546e30
henryhall897 <79281394+henryhall897@users.noreply.github.com>
2023-03-31 02:58:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const Route = require('./routes/mainRoute');
 
+
+//amazon_cognito dependencies. 
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const cognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+const got = require('got');
+const jwkToPem = require('jwk-to-pem');
+const jwt = require('jsonwebtoken');
+global.fetch = require('node-fetch');
+
+
 app = express();
 
 app.set('view engine', 'ejs');
@@ -21,7 +32,22 @@ app.get('/', (req, res)=> {
 
 app.use('/', Route);
 
-
+app.post('/create')
 app.listen(port, ()=> {
     console.log('Server is running on port', port);
 });
+
+//aws cognito functions
+const poolData = {
+    UserPoolId: "us-east-1_rAjVncXnw",
+    ClientId: "ovs2ojp2n3rt48b7jqcad7kng"
+};
+
+const pool_region = 'us-east-1';
+
+const userPool = new AmazonCognitoIdentity.cognitoUserPool(poolData);
+
+function RegisterUser(username, email){
+    let attributeList = [];
+    attributeList.push
+}
