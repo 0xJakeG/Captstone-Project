9b22969706dc90d048148b1ed10ad9336dc3174c
leeCaesar <89537085+leeCaesar@users.noreply.github.com>
2023-03-31 17:02:15 -0400

    connect sql

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql');
 const methodOverride = require('method-override');
 const Route = require('./routes/mainRoute');
 const db = require('./sequelize/models');
@@ -19,11 +20,32 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 app.use('/images', express.static('/images'));
+app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'))
 app.use(methodOverride('_method'));
 
 var port = process.env.PORT || 8080; // set the port
+
+//Connect to database
+var config = mysql.createConnection({
+    user: 'root',
+    password: '69LgU84Bta8RZJr',
+    host: 
+    'awseb-e-epz4ed3tmg-stack-awsebrdsdatabase-uz3xxyihfosx.cs6g7v4x3uz2.us-east-1.rds.amazonaws.com',
+    database: 'booksforcooks'
+});
+app.get('/', function(req, res) {
+    config.connect(function(err) {
+        var data = {};
+        if(err) console.log(err);
+        config.query('SELECT * FROM Testrecipes', function(err, result) {
+            if(err) console.log(err);
+            data = {print: result};
+            res.render('index', {data: data});
+        });
+    });
+});
 
 app.get('/', (req, res)=> {
     res.render('index');
