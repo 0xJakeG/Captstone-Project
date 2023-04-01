19ca00a7e7d46af3519a34ebb469c2f1b592c587
0xJakeG <jakemray2020@gmail.com>
2023-04-01 16:13:44 -0400

    change to app.js from what is in master

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql');
 const methodOverride = require('method-override');
 const Route = require('./routes/mainRoute');
-const db = require('./sequelize/models');
-const { request } = require('express');
 const bodyParser = require('body-parser');
-const userModel = require('./sequelize/models/user');
 
-const {user} = require ("./sequelize/models");
-require('dotenv').config();
-
-const {recipe} = require("./sequelize/models");
-require('dotenv').config()
 
 app = express();
 
@@ -19,11 +12,34 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 app.use('/images', express.static('/images'));
+app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'))
 app.use(methodOverride('_method'));
 
+
+
 var port = process.env.PORT || 8080; // set the port
+
+//Connect to database
+var config = mysql.createConnection({
+    user: 'JakeAdmin',
+    password: '69LgU84Bta8RZJr',
+    host: 
+    'awseb-e-epz4ed3tmg-stack-awsebrdsdatabase-uz3xxyihfosx.cs6g7v4x3uz2.us-east-1.rds.amazonaws.com',
+    database: 'booksforcooks'
+});
+app.get('/allRecipes', function(req, res) {
+    config.connect(function(err) {
+        var data = {};
+        if(err) console.log(err);
+        config.query('SELECT * FROM Testrecipes', function(err, result) {
+            if(err) console.log(err);
+            data = {print: result};
+            res.render('allRecipes', {data: result});
+        });
+    });
+});
 
 app.get('/', (req, res)=> {
     res.render('index');
@@ -31,23 +47,7 @@ app.get('/', (req, res)=> {
 
 app.use('/', Route);
 
-app.post('/create', (req, res) => {
-    user.create({firstName: req.body.name, email: req.body.email, password: req.body.password}) //.then(user => res.json(user))
-})
 
-app.post('/create-recipe', (req, res) => {
-    recipe.create({recipes_id: 1, 
-                   recipes_name: req.body.recipe_name, 
-                   recipes_description: req.body.description, 
-                   postedBy: "your mom", 
-                   mealType: "Standard", 
-                   timeToCompletion: 20, 
-                   image: "Image"})
-    console.log(request.body.title);
-})
-
-db.sequelize.sync().then((req)=> {
-    app.listen(port, ()=> {
-        console.log('Server is running on port', port);
-    });
+app.listen(port, ()=> {
+    console.log('Server is running on port', port);
 });
