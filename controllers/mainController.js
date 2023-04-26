0c587a1570167e39cfae57ec55b853e4435066ca
Jake <110588119+0xJakeG@users.noreply.github.com>
2023-04-26 18:04:01 -0400

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,5 @@
+const { config } = require('../app');
+
 exports.index = (req, res, next)=> {
     res.render('../views/index');
 }
@@ -29,6 +31,38 @@ exports.map = (req, res, next)=> {
 exports.allRecipes = (req, res, next)=> {
     res.render('../views/allRecipes');
 }
+
+exports.recipeDetails = async (req, res, next) => {
+    console.log(req.params);
+    let id = req.params.recipe_id;
+    try {
+      const recipe = await getRecipeById(id, req);
+      if (recipe) {
+        res.render('recipeDetails', { recipe });
+      } else {
+        let err = new Error('Cannot find a recipe with id ' + id);
+        err.status = 404;
+        next(err);
+      }
+    } catch (error) {
+      console.error('Error retrieving recipe details:', error);
+      res.status(500).send('Error retrieving recipe details');
+    }
+  };
+
+  // Modify the getRecipeById function
+function getRecipeById(id, req) {
+    return new Promise((resolve, reject) => {
+      req.config.query('SELECT * FROM recipes WHERE recipe_id = ?', [id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result[0]);
+        }
+      });
+    });
+  }
+  
 
 exports.showRecipe = (req, res, next)=> {
     let id = req.params.id;
