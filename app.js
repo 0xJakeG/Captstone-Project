e796570b994297281c286b68a1e59849d9805df1
0xJakeG <jakemray2020@gmail.com>
2023-04-26 23:50:58 -0400

    add instuctions and picture

--- app.js
+++ app.js
@@ -71,13 +71,14 @@ app.post('/addRecipe', (req, res) => {
       instructionDescription
     } = req.body;
   
+    
     const recipeSql =
       'INSERT INTO recipes (recipe_name, recipe_type, recipe_description, recipe_picture) VALUES (?, ?, ?, ?)';
     const ingredientSql =
       'INSERT INTO recipe_ingredients (recipe_id, ingredient_name, measurement_qty, measurement_unit) VALUES (?, ?, ?, ?)';
     
-      const instructionSql =
-      'INSERT INTO instructions (recipe_id, instruction_description) VALUES (?, ?)';
+    const instructionSql =
+        'INSERT INTO instructions (recipe_id, instruction_description, order_number) VALUES (?, ?, ?)';
   
     config.query(
       recipeSql,
@@ -109,17 +110,17 @@ app.post('/addRecipe', (req, res) => {
         }
 
         for (let j = 0; j < instructionDescription.length; j++) {
-          config.query(instructionSql,[recipeId,instructionDescription[j]],
-            (error, results, fields) => {
-              if (error) {
-                console.error(error);
-                res.sendStatus(500);
-                return;
+            config.query(instructionSql,[recipeId, instructionDescription[j], j + 1],
+              (error, results, fields) => {
+                if (error) {
+                  console.error('Error inserting instruction:', error);
+                  res.sendStatus(500);
+                  return;
+                }
+                console.log(results);
               }
-              console.log(results);
-            }
-          );
-        }
+            );
+          }
   
         res.sendStatus(200);
       }
